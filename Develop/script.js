82c75bd1ce010903d44d081077f79b630f41c3c5
Benito <benitocavazosr@yahoo.com>
2021-12-10 02:28:36 -0600

    saving progress

--- Develop/script.js
+++ Develop/script.js
@@ -1,28 +1,8 @@
-vas dSchedull {};
 
-var loaddSchedull = function() {
-    dSchedull = JSON.parse(localStorage.getItem("dSchedull"));
-  
-    // if nothing in localStorage, create a new object to track all task status arrays
-    if (!dSchedull) {
-      dSchedull = {
-        toDo: [],
-        inProgress: [],
-        inReview: [],
-        done: []
-      };
-    }
-  
-    // loop over object properties
-    $.each(dSchedull, function(list, arr) {
-      console.log(list, arr);
-      // then loop over sub-array
-      arr.forEach(function(task) {
-        createTask(task.text, task.date, list);
-      });
-    });
-  };
 
-// load Schedulle for the first time
-loaddSchedull();
 
+$("#9amBtn").on("click", function(){
+    var text = $("#text9am").val();
+    localStorage.setItem("9am", text);
+    console.log(text);
+});
